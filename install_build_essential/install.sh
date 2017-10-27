b63397c10a84e6538ef7bf7f9f924b59daf2e486
mooring <mooring@live.com>
2017-10-27 13:55:42 +0800

    install

--- install_build_essential/install.sh
+++ install_build_essential/install.sh
@@ -4,6 +4,10 @@ chmod +x ./unzip
 find . -regex '.*\.zip' -exec sh -c "./unzip -o \"{}\" -d ~/" \;
 if [ -f ~/cctools/bin/set-default-compiler-clang ]; then
     sed -i 's/\${CCTOOLSDIR}/~\/cctools/g' ~/cctools/bin/*
+    sed -i 's/com.pdaxrom.cctools\/root/com.spartacusrex.spartacuside/g' ~/cctools/bin/*
     cd ~/cctools/bin && sh ~/cctools/bin/set-default-compiler-clang
 fi
+ln -sf ~/cctools/bin/aclocal-1.14 ~/cctools/bin/aclocal
+ln -sf ~/cctools/bin/automake-1.14 ~/cctools/bin/automake
+
 rm -f ~/pkgdesc ~/postinst ~/prerm &>/dev/null
