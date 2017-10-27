902aebc85800e064b7dbfbe6a83259430acd073d
mooring <mooring@live.com>
2017-10-27 11:36:55 +0800

    update

--- install_gcc/install.sh
+++ install_gcc/install.sh
@@ -6,9 +6,9 @@ find . -regex '.*\.zip' -exec sh -c "./unzip -oq \"{}\" -d ~/" \;
 
 export PATH=~/cctools/bin
 export CCTOOLSDIR=~/cctools
-sh ~/cctools/postinst
 sh ~/cctools/prerm
-
+sh ~/cctools/postinst
+rm -f ~/cctools/postinst ~/cctools/prerm
 
 sed -i '/cctools/d' ~/.bash_aliases
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/cctools/lib' >> ~/.bash_aliases
