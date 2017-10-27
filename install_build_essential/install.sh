a30b15d18a1a2b51cb77987105aee2e11fde6d31
mooring <mooring@live.com>
2017-10-27 15:53:43 +0800

    install.sh

--- install_build_essential/install.sh
+++ install_build_essential/install.sh
@@ -6,7 +6,5 @@ if [ -f ~/cctools/bin/set-default-compiler-clang ]; then
     sed -i 's/\${CCTOOLSDIR}/~\/cctools/g' ~/cctools/bin/*
     cd ~/cctools/bin && sh ~/cctools/bin/set-default-compiler-clang
 fi
-ln -sf ~/cctools/bin/aclocal-1.14 ~/cctools/bin/aclocal
-ln -sf ~/cctools/bin/automake-1.14 ~/cctools/bin/automake
 
 rm -f ~/pkgdesc ~/postinst ~/prerm &>/dev/null
