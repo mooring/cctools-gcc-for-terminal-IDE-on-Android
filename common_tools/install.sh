3c376c5c8e2a84bd4eefbc35afaa99e50d8aebd1
mooring <mooring@live.com>
2017-10-27 12:56:53 +0800

    update

--- common_tools/install.sh
+++ common_tools/install.sh
@@ -2,4 +2,5 @@
 
 chmod +x ./unzip
 find . -regex '.*\.zip' -exec sh -c "./unzip -o \"{}\" -d ~/" \;
+sed -i 's/\${CCTOOLSDIR}/~\/cctools/g' ~/cctools/bin/*
 rm -f ~/pkgdesc ~/postinst ~/prerm &>/dev/null
